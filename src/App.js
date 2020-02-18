7288b2859080e79ff3c7cc24861e4bd9936c23bb
Samuel Hodges <samuel.hodges@4mations-MacBook-Pro.local>
2020-02-18 16:16:47 +1100

    Remove socket connection from app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import axios from 'axios';
 import './App.css';
 import store from './store';
 import { Provider } from 'react-redux';
+import axios from 'axios'
 //Components
 import Dashboard from './components/dashboard/Dashboard';
 import Login from './components/auth/Login';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import io from 'socket.io-client';
 
 const theme = createMuiTheme({
   palette: {}
@@ -20,17 +19,6 @@ if (localStorage.token) {
 } else {
   delete axios.defaults.headers.common['Auth'];
 }
-
-const socket = io('https://revisioncheck.herokuapp.com/');
-socket.on('connect', function(soc) {
-  socket.on('connection:sid', function(socketId) {
-    localStorage.socketId = socketId;
-  });
-
-  socket.on('folder', function(folder) {
-    console.log(folder);
-  });
-});
 
 const App = () => {
   return (
